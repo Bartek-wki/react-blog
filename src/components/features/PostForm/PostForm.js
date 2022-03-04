2b72f12ea9b16f57c61187a4025ebe62fcf31f4d
Bartosz Wilki <b.wilki0109@gmail.com>
2022-03-04 17:17:36 +0100

    20.3 v1

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -11,8 +11,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 //CSS Modules, react-datepicker-cssmodules.css
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-import { dateToStr } from '../../../utils/dateToStr';
-
+import { useForm } from "react-hook-form";
 
 const PostForm = ({ action, actionText, initialTitle, initialAuthor, initialPublishedDate, initialShortDescription, initialContent}) => {
   
@@ -21,29 +20,55 @@ const PostForm = ({ action, actionText, initialTitle, initialAuthor, initialPubl
   if (initialPublishedDate === undefined) {initialPublishedDate = ''}
   if (initialShortDescription === undefined) {initialShortDescription = ''}
   if (initialShortDescription === undefined) {initialShortDescription = ''}
-  if (initialContent === undefined) {initialContent = ''}
+  if (initialContent === undefined) { initialContent = '' }
+  
+  const { register, handleSubmit: validate, formState: { errors } } = useForm();
   
   const [title, setTitle] = useState(initialTitle)
   const [author, setAuthor] = useState(initialAuthor)
   const [shortDescription, setShortDescription] = useState(initialShortDescription)
   const [publishedDate, setPublishedDate] = useState(initialPublishedDate)
   const [content, setContent] = useState(initialContent)
-  
-  const handleSubmit = e => {
-    e.preventDefault();
-    action({ title, author, publishedDate, shortDescription, content})
+  const [contentError, setContentError] = useState(false)
+  const [dateError, setDateError] = useState(false)
+
+  const handleSubmit = () => {
+    setContentError(!content)
+    setDateError(!publishedDate)
+    if (content && publishedDate) {
+      action({ title, author, publishedDate, shortDescription, content })
+    }
   }
  
   return (
-    <Form className="col-10" onSubmit={handleSubmit}>
+    <Form className="col-10" onSubmit={validate(handleSubmit)}>
       <Row className="d-flex flex-column">
-        <FormGroupText label={"Title"} placeholder={"Enter title"} value={title} setValue={setTitle} />
-        <FormGroupText label={"Author"} placeholder={"Enter author"} value={author} setValue={setAuthor} />
+        <FormGroupText
+          useFormRegister={{ ...register("title", { required: true, minLength:3 }) }}
+          useFormError={errors}
+          ErrorText={"This field requires at least 3 characters "}
+          label={"Title"}
+          placeholder={"Enter title"}
+          value={title} setValue={setTitle} />
+        <FormGroupText
+          useFormRegister={{ ...register("author", { required: true, minLength:3 }) }}
+          useFormError={errors}
+          ErrorText={"This field requires at least 3 characters "}
+          label={"Author"}
+          placeholder={"Enter author"}
+          value={author} setValue={setAuthor} />
         <DatePicker selected={publishedDate} onChange={(date) => setPublishedDate(date)} />
-        <FormGroupTextarea label={"Short description"} height={"100px"} value={shortDescription} setValue={setShortDescription} />
+        {dateError && <small className="d-block form-text text-danger mt-2">Date can't be empty</small>}
+        <FormGroupTextarea
+          useFormRegister={{ ...register("shortDescription", { required: true, minLength:20 }) }}
+          useFormError={errors}
+          ErrorText={"This field requires at least 20 characters "}
+          label={"Short description"} height={"100px"}
+          value={shortDescription} setValue={setShortDescription} />
         <ReactQuill theme="snow" value={content || ''} onChange={setContent} />
+        {contentError && <small className="d-block form-text text-danger mt-2">Content can't be empty</small>}
       </Row>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" className="my-4">
         {actionText}
       </Button>
     </Form>
