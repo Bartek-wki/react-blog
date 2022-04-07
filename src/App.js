af592e48ccb81c00a9cc0f5fcd878cd61edd71ec
Bartosz Wilki <b.wilki0109@gmail.com>
2022-04-07 14:44:37 +0200

    test

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
   return (
     <Container>
       <Header />
+      <h1>Hello</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<Post />} />
